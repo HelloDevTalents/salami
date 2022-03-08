4859f7c51d62bbbeb4d09044f2b61669d7b35e77
Piotr Sędek <piotrseed@gmail.com>
2022-03-08 13:19:23 +0100

    feat(package): create salami theme package

---
+++ salami_theme/lib/salami_theme.dart
@@ -0,0 +1,8 @@
+library salami_theme;
+
+export 'src/colors/colors.dart';
+export 'src/dimensions/dimensions.dart';
+export 'src/generated/generated.dart';
+export 'src/icons/icons.dart';
+export 'src/typography/typography.dart';
+export 'src/widgets/widgets.dart';
---
+++ salami_theme/lib/src/colors/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class SalamiColors {
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const transparent = Colors.transparent;
+
+  static const gunmetal = Color(0xFF022B3A);
+  static const metallicSaeweed = Color(0xFF1F7A8C);
+  static const beaBlue = Color(0xFFBFDBF7);
+  static const lavenderWeb = Color(0xFFE1E5F2);
+
+  static const grey1 = Color(0xFF141414);
+  static const grey2 = Color(0xFF707070);
+  static const grey3 = Color(0xFFb4b4b4);
+  static const grey4 = Color(0xFFdcdcdc);
+  static const grey5 = Color(0xFFe9e9e9);
+  static const grey6 = Color(0xFFf7f7f7);
+}
