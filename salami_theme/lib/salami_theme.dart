326fa9323b67f941eae67dd5d98be31cba1f4274
Piotr Sędek <piotrseed@gmail.com>
2022-03-09 10:17:28 +0100

    refactor(samlami package): remove old files

--- salami_theme/lib/salami_theme.dart
+++
@@ -1,8 +0,0 @@
-library salami_theme;
-
-export 'src/colors/colors.dart';
-export 'src/dimensions/dimensions.dart';
-export 'src/generated/generated.dart';
-export 'src/icons/icons.dart';
-export 'src/typography/typography.dart';
-export 'src/widgets/widgets.dart';
--- salami_theme/lib/src/typography/typography.dart
+++
@@ -1,1 +0,0 @@
-export 'salami_font.dart';
