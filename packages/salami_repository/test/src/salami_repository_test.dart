c9647b9c0aa28860ff2f87b36d4d2803c4d7b075
l-borkowski <lukasz.borkowski@unitedideas.pl>
2022-03-10 13:14:47 +0100

    feat: create salami repository package with example

---
+++ packages/salami_repository/test/src/salami_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter_test/flutter_test.dart';
+import 'package:salami_repository/salami_repository.dart';
+
+void main() {
+  group('SalamiRepository', () {
+    test('can be instantiated', () {
+      expect(SalamiRepository(), isNotNull);
+    });
+  });
+}
